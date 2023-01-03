d2951b2a7f6680c54cfd7a4b5ffd7411a13493a2
Rishav <rishavj032@gmail.com>
2023-01-03 17:07:34 +0530

    JS-402-02

---
+++ JS-402-02/script.js
@@ -0,0 +1,7 @@
+      function hello(){
+        let h2 = document.getElementById('imageadded')
+        h2.innerHTML = "Image Element Added.";
+        var image = document.getElementById('image')
+        image.src = "https://mybigplunge.com/wp-content/uploads/2021/12/Geekster_logo.jpg"
+      }
+  
